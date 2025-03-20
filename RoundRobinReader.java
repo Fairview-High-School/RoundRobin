6a6953ca6317df281d13fe60a0af40a5650121f6
Tim Gesell <timothy.gesell@bvsd.org>
2025-03-20 10:27:43 -0600

    Starter Files

--- RoundRobinReader.java
+++ RoundRobinReader.java
@@ -1,29 +1,35 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.Scanner;
 
 public class RoundRobinReader {
 	
 	private Queue<Process> processQueue;
 	private int quantumLength; 
-	
-	
+
 	public RoundRobinReader(String filename)
 	{
-		Scanner file = new Scanner(new File(filename));
-		
-		/*Read the number of processes included in the file, the quantum length to be used, and
-		 * the information about each process, from the data file.
-		 * The information about each data file is read into a Queue of Process objects
-		 */
-        int processNum = Integer.parseInt(file.nextLine());
-        quantumLength = Integer.parseInt(file.nextLine());
-        processQueue = new LinkedList<>();
-        while(file.hasNextLine()) {
-            String[] line = file.nextLine().split(",");
-            processQueue.add(new Process(Integer.parseInt(line[0]),line[1],Integer.parseInt(line[2])));
-        }
-        file.close();
+		Scanner file = null;
+		try
+		{
+			file = new Scanner(new File(filename));
+			/*Read the number of processes included in the file, the quantum length to be used, and
+			 * the information about each process, from the data file.
+			 * The information about each data file is read into a Queue of Process objects
+			 */
+			int processNum = Integer.parseInt(file.nextLine());
+			quantumLength = Integer.parseInt(file.nextLine());
+			processQueue = new Queue<>();
+			while(file.hasNextLine()) {
+				String[] line = file.nextLine().split(",");
+				processQueue.enqueue(new Process(Integer.parseInt(line[0]),line[1],Integer.parseInt(line[2])));
+			}
+			file.close();
+		}
+		catch (FileNotFoundException e) {
+			System.err.println(e.getMessage());
+			throw new RuntimeException(e);
+		}
 	}
 	
 	public RoundRobinReader()
@@ -38,7 +44,4 @@ public class RoundRobinReader {
 	public int getQuantumLength() {
 		return quantumLength;
 	}
-	
-    
-
 }
